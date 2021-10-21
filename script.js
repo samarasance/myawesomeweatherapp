8b84861e14c3ceacc8d1b0ca5ee7b1e4e4a10e0f
samarasance <88958996+samarasance@users.noreply.github.com>
2021-10-20 20:35:29 -0500

    improvement to JS

--- script.js
+++ script.js
@@ -27,12 +27,7 @@ function displayForcast() {
   let forcastElement = document.querySelector("#forcast");
 }
 
-function formatDay(timestamp) {
-  let date = new Date(timestamp * 1000);
-  let day = date.getDay();
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  return days[day];
-}
+let days = ["Thu", "Fri", "Sat", "Sun"];
 
 let forecastHTML = `<div class="row">`;
 forecast.forEach(function (forecastDay, index) {
@@ -50,12 +45,8 @@ forecast.forEach(function (forecastDay, index) {
           width="42"
         />
         <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> ${Math.round(
-            forecastDay.temp.max
-          )}° </span>
-          <span class="weather-forecast-temperature-min"> ${Math.round(
-            forecastDay.temp.min
-          )}° </span>
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
         </div>
       </div>
   `;
@@ -65,16 +56,9 @@ forecast.forEach(function (forecastDay, index) {
 forecastHTML = forecastHTML + `</div>`;
 forecastElement.innerHTML = forecastHTML;
 
-function getForecast(coordinates) {
-  console.log(coordinates);
-  let apiKey = "292929ff665169ef5a98dcc8cc29979a";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayForecast);
-}
-
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
-  let cityElement = document.querySelector(".city");
+  let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
@@ -91,34 +75,41 @@ function displayTemperature(response) {
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
   iconElement.setAttribute("alt", response.data.weather[0].description);
-
-  getForecast(response.data.coord);
 }
 
-function search(event) {
-  event.preventDefault();
-  let cityInputElement = document.querySelector("#city-input");
-  console.log(cityInputElement.value);
+function search(city) {
+  let apiKey = "292929ff665169ef5a98dcc8cc29979a";
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
 }
-
-let apiKey = "292929ff665169ef5a98dcc8cc29979a";
-let city = "Washington DC";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-axios.get(apiUrl).then(displayTemperature);
-
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", search);
 
 function handleSubmit(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector(".city-input");
   search(cityInputElement.value);
-}
 
-function displayFahrenheitTemperature(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
+  function getForecast(coordinates) {
+    console.log(coordinates);
+    let apiKey = "292929ff665169ef5a98dcc8cc29979a";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
+  }
+
+  function search(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    console.log(cityInputElement.value);
+  }
+
+  function displayFahrenheitTemperature(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+
+    let form = document.querySelector("#search-form");
+    form.addEventListener("submit", search);
+  }
 
   celsiusLink.classList.remove("active");
   fahrenheitLink.classList.add("active");
