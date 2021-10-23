5a42de1852e1974174fbb75a2ad91c17861e8050
samarasance <88958996+samarasance@users.noreply.github.com>
2021-10-23 16:11:33 -0500

    added date js

--- src/script.js
+++ src/script.js
@@ -1,14 +1,20 @@
+function formatDate(timestamp) {
+  return "Saturday 04:43 pm";
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
-  windElement.innerHTML = response.data.wind.speed;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "292929ff665169ef5a98dcc8cc29979a";
