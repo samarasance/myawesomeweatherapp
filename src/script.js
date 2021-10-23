f188b0e9e8a2e950972c86e842af565f18ddedf1
samarasance <88958996+samarasance@users.noreply.github.com>
2021-10-23 15:35:18 -0500

    added current condition to JS

--- src/script.js
+++ src/script.js
@@ -2,9 +2,13 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = response.data.wind.speed;
 }
 
 let apiKey = "292929ff665169ef5a98dcc8cc29979a";
